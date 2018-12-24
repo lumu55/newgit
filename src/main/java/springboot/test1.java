6a6b851ea148140c92d92f1d846d0b05056b3280
lm <1760532400@qq.com>
2018-12-24 11:19:50 +0800

    321

---
+++ src/main/java/springboot/test1.java
@@ -0,0 +1,10 @@
+package springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class test1 {
+	public static void main(String[] args) {
+	
+	}
+}
